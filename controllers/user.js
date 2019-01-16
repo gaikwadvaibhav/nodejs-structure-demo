55a5dbebe5b8af03b34362df58122923a7c71f84
Vaibhav Gaikwad <vaibhav@amdev.in>
2019-01-16 18:38:50 +0530

    file upload, sendgrid email sending and jwt token

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,13 @@ var passwordHash = require('password-hash');
 
 var User = require('../mongo-models/user');
 
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey('your_api_key');
+
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var config = require('../config/config');
+
 module.exports = () => {
     var user = {};
 
@@ -16,7 +23,13 @@ module.exports = () => {
                     contact: req.body.contact,
                     password: password
                 }
-                var user = new User(obj);   
+                var user = new User(obj);
+
+                // create a token
+                var token = jwt.sign({ id: user._id }, config.secret, {
+                    expiresIn: 86400 // expires in 24 hours
+                });
+                console.log('token---->', token)
 
                 user.save((err, result) => {
                     console.log('result', result)
@@ -25,7 +38,17 @@ module.exports = () => {
                         res.json({ success: false, message: "Error in saving data" })
                     }
                     else {
-                        res.json({ success: true, message: "User created successfully", data: result })
+                        // send mail using sendgrid
+                        const msg = {
+                            to: 'test1@dev.in',
+                            from: 'test2@dev.in',
+                            subject: 'Sending with SendGrid is Fun',
+                            text: 'and easy to do anywhere, even with Node.js ',
+                            html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+                        };
+                        sgMail.send(msg);
+
+                        res.json({ success: true, message: "User created successfully", data: result, token: token })
                     }
                 })
             }
@@ -35,17 +58,42 @@ module.exports = () => {
         })
     },
 
-    user.getAllUser = (req, res) => {
-        console.log('reqbody', req)
-        User.find((err, result) => {
-            console.log('users res------------>>>>', res)
-            if(res) {
-                res.json({ success: true, message: "Users data fetch successfully", data: result })
-            } else {
-                res.json({ success: false, message: "Error in fetching users data" })
+        user.getAllUser = (req, res) => {
+            console.log('reqbody', req)
+            User.find((err, result) => {
+                console.log('users res------------>>>>', res)
+                if (res) {
+                    res.json({ success: true, message: "Users data fetch successfully", data: result })
+                } else {
+                    res.json({ success: false, message: "Error in fetching users data" })
+                }
+            })
+        },
+
+        user.fileUpload = (req, res, next) => {
+
+            console.log('req', req.file)
+            var obj = {
+                success: true,
+                message: 'file upload success'
             }
-        })
-    }
+            res.json(obj)
+
+        },
+
+        user.getSingleUser = (req, res) => {
+            console.log('req', req)
+            var token = req.headers['x-access-token'];
+            console.log('----token----', token)
+
+            if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+
+            jwt.verify(token, config.secret, function (err, decoded) {
+                if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+
+                res.status(200).send(decoded);
+            });
+        }
 
     return user;
 }
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,36 @@ var router = express.Router();
 
 var userController = require('../controllers/user')();
 
+// var multer = require('multer');
+
+// var storage = multer.diskStorage({
+//     destination: function (req, file, callback) {
+//         callback(null, './uploads');
+//     },
+//     filename: function (req, file, callback) {
+//         var datetimestamp = Date.now();
+//         const fileName = file.originalname.split('.').slice(0, -1).join('.')
+//         const extention = file.originalname.substr((Math.max(0, file.originalname.lastIndexOf(".")) || Infinity) + 1)
+//         callback(null, fileName + "-" + datetimestamp + "." + extention);
+//     }
+// });
+
+// var upload = multer({ storage: storage })
+
+var multer  = require('multer');
+var upload = multer({ dest: 'upload/'});
+
+var fs = require('fs');
+
 router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
 });
 
 router.post('/api/createuser', userController.createUser);
 router.get('/api/getusers', userController.getAllUser);
+router.post('/api/fileupload', upload.single('recfile'), userController.fileUpload)
+// upload.array('recfile', 10) // for multiple file uploads 
+router.get('/api/getsingleuser', userController.getSingleUser);
+
 
 module.exports = router;
